2371baeb18c419e2aff1340527c4a379dbb73e98
bendoane <ben.doane@gmail.com>
2020-09-10 15:12:33 -0400

    first push

---
+++ grammarTypes/01-comments.js
@@ -0,0 +1,19 @@
+/*
+* ************
+! Comments
+* ************
+*/
+
+// Single Line comment
+
+//quick note
+
+// here is my quick note
+
+/* Multi line comments
+- point one
+- point two
+- point three
+- ect.
+*/
+
